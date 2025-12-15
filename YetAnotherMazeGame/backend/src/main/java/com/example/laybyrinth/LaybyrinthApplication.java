785c799aa01317ff9568016e2ba2177ba1e81915
cujhelyi <cujhelyi@hotmail.com>
2025-12-15 02:46:32 -0500

    Final migration completion: Add LaybyrinthApplication and tests

---
+++ YetAnotherMazeGame/backend/src/main/java/com/example/laybyrinth/LaybyrinthApplication.java
@@ -0,0 +1,13 @@
+package com.example.laybyrinth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaybyrinthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaybyrinthApplication.class, args);
+	}
+
+}
---
+++ YetAnotherMazeGame/backend/src/test/java/com/example/laybyrinth/LaybyrinthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laybyrinth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaybyrinthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
